cb5dc12d743c15d0b785bf028afca8962d6559e8
Miguel Sousa <migasvsousa@gmail.com>
2025-12-23 19:39:57 +0000

    Progress Bar

--- Assets/Scripts/RollingGroundScript.cs
+++ Assets/Scripts/RollingGroundScript.cs
@@ -1,22 +1,69 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-// Este script cria um chão novo quando o player passa por um trigger presente no chão,
-// o que cria um loop infinito
 public class RollingGroundScript : MonoBehaviour
 {
     public GroundLevelData levelData;
-
     public GameObject ground;
     public GameObject gate1;
     public GameObject gate2;
     public GameObject youWin;
 
+    [Header("Progress UI")]
+    public Slider progressSlider;
+    public float fillSpeed = 2f; //for the progress bar
+
     private int groundCount;
+    private float targetSliderValue;
+    private bool isUpdatingSlider;
 
     private void Start()
     {
         groundCount = 0;
+        targetSliderValue = 0;
+        isUpdatingSlider = false;
+        InitializeProgressSlider();
+    }
+
+    private void Update()
+    {
+        if (isUpdatingSlider && progressSlider != null)
+        {
+            progressSlider.value = Mathf.Lerp(progressSlider.value, targetSliderValue, Time.deltaTime * fillSpeed);
+
+            if (Mathf.Abs(progressSlider.value - targetSliderValue) < 0.01f)
+            {
+                progressSlider.value = targetSliderValue;
+                isUpdatingSlider = false;
+            }
+        }
+    }
+
+    private void InitializeProgressSlider()
+    {
+        if (progressSlider != null)
+        {
+            progressSlider.minValue = 0;
+            progressSlider.maxValue = levelData.totalGroundCount;
+            progressSlider.value = 0;
+        }
+        else
+        {
+            Debug.LogWarning("where progress bar bro?");
+        }
+    }
+
+    private void UpdateProgressSlider()
+    {
+        if (progressSlider != null)
+        {
+            targetSliderValue = groundCount;
+            isUpdatingSlider = true;
+
+            float percentage = (float)groundCount / levelData.totalGroundCount * 100f;
+            Debug.Log($"Progresso: {groundCount}/{levelData.totalGroundCount} ({percentage:F1}%)");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -45,36 +92,28 @@ public class RollingGroundScript : MonoBehaviour
             youWin.SetActive(true);
 
         groundCount++;
+        UpdateProgressSlider();
     }
 
-
-
-
-private void SpawnGate(int buttonsNum, int buttonsHp)
+    private void SpawnGate(int buttonsNum, int buttonsHp)
     {
-
         switch (buttonsNum)
         {
-
             case 0:
                 Instantiate(ground);
                 break;
+
             case 1:
                 GameObject gateOne = Instantiate(gate1);
-
                 Transform blueSquare = gateOne.transform.Find("Gate/Blue Square");
-
                 EnemyMoveScript script = blueSquare.GetComponent<EnemyMoveScript>();
-
                 script.maxHealth = buttonsHp;
                 script.SetHealthToMax();
                 script.enabled = false;
-
                 break;
 
             case 2:
                 GameObject gateTwo = Instantiate(gate2);
-
                 Transform blueSquare2 = gateTwo.transform.Find("Gate/Blue Square");
                 Debug.Log(blueSquare2);
                 EnemyMoveScript script2 = blueSquare2.GetComponent<EnemyMoveScript>();
@@ -86,14 +125,12 @@ private void SpawnGate(int buttonsNum, int buttonsHp)
                 EnemyMoveScript script3 = blueSquare3.GetComponent<EnemyMoveScript>();
                 script3.maxHealth = buttonsHp;
                 script3.SetHealthToMax();
-
-
-
                 break;
 
             default:
                 Instantiate(ground);
                 break;
         }
-    } 
+    }
 }
+
