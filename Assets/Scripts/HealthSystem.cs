8d337735ef8710907da25a1b8d583d567421184a
Miguel Sousa <migasvsousa@gmail.com>
2026-01-17 06:08:23 +0000

    Player Flashes When Damaged

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -11,6 +11,13 @@ public class HealthSystem : MonoBehaviour
     public int minHealth = 0;
     public int damage = 1;
 
+    public float flashDuration = 0.1f;
+    public Color flashColor = Color.red;
+
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+    private bool isFlashing = false;
+
     public void GameOver() {  
 
         //death sfx
@@ -21,6 +28,11 @@ public class HealthSystem : MonoBehaviour
     private void Start()
     {
         health = maxHealth;
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            originalColor = spriteRenderer.color;
+        }
     }
 
     public void TakeDamage(int damage)
@@ -28,6 +40,11 @@ public class HealthSystem : MonoBehaviour
         health -= damage;
         AudioSource sfx = GameObject.Find("DamagedSFX").GetComponent<AudioSource>();
         sfx.Play();
+
+        if (spriteRenderer != null && !isFlashing)
+        {
+            StartCoroutine(DamageFlash());
+        }
 
         if (health < 9000) { 
         Opacity opacityScript = this.GetComponent<Opacity>();
@@ -41,4 +58,21 @@ public class HealthSystem : MonoBehaviour
             GameOver();
         }
     }
+
+    private IEnumerator DamageFlash()
+    {
+        isFlashing = true;
+
+        // Flash red
+        spriteRenderer.color = flashColor;
+
+        // Wait for flash duration
+        yield return new WaitForSeconds(flashDuration);
+
+        // Return to original color
+        spriteRenderer.color = originalColor;
+
+        isFlashing = false;
+    }
+
 }
